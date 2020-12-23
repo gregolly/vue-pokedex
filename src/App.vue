b2956a6d9af6dd5b5177b8aa0a5ac36012ed21b8
Gregolly frança <42480355+gregolly@users.noreply.github.com>
2020-12-22 21:28:17 -0400

    Pokedex para busca de pokemons.

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,68 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div class="column is-half is-offset-one-quarter">
+      <h1 class="title is-1">Pokedex</h1>
+      <input
+      class="input is-primary" 
+      type="text"
+      name=""
+      id=""
+      placeholder="Buscarpokemon..."
+      v-model="search"
+      >
+      <button @click="searchPokemon" class="button is-primary is-fullwidth mt-3" id="buscaBtn">Buscar</button>
+      <div >
+        <div v-for="(pokemon, index) in filteredPokemons" :key="pokemon.url">
+          <Pokemon :name="pokemon.name" :url="pokemon.url"
+          :num="index+1"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import axios from 'axios'
+import Pokemon from './components/Pokemon'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  components:{
+    Pokemon
+  },
+  data(){
+    return{
+       pokemons: [],
+       filteredPokemons: [],
+       search: ''
+    }
+  },
+  created(){
+    axios.get('https://pokeapi.co/api/v2/pokemon?limit=151&offset=0')
+    .then(res => {
+      console.log(res.data.results)
+      this.pokemons = res.data.results
+      this.filteredPokemons = res.data.results
+    })
+  },
+  methods:{
+    searchPokemon(){
+      this.filteredPokemons = this.pokemons
+        if(this.search == '' || this.search == ' '){
+          this.filteredPokemons = this.pokemons
+        } else {
+          this.filteredPokemons = this.pokemons.filter(pokemon => pokemon.name == this.search)
+        }
+    }
+  },
+  computed:{
+    // resultSearch(){
+    //   if(this.search == '' || this.search == ' '){
+    //     return this.pokemons
+    //   } else {
+    //     return this.pokemons.filter(pokemon => pokemon.name == this.search)
+    //   }
+    // }
   }
 }
 </script>
